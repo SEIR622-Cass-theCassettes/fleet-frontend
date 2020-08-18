12013fa6b86ff12c24937adf6cf5c9ff35ac2f0d
cassioblu55 <cassioblubyrd@gmail.com>
2020-08-17 21:36:36 -0500

    Fix evn param

--- src/api/FleetBackend.js
+++ src/api/FleetBackend.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const backendUrl = process.env.REACT_APP_IS_LOCAL
 	? 'http://localhost:8000/api'
-	: process.env.BACKEND_URL;
+	: process.env.REACT_APP_BACKEND_URL;
 
 function FleetBackend() {
 	return axios.create({
