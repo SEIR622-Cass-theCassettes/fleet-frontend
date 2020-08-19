1e616830b6021853baaf4277748d7feda522adf0
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-08-18 22:41:51 -0700

    modal nearly completed

--- src/TruckList.js
+++ src/TruckList.js
@@ -5,44 +5,173 @@ import truckimage from './truck.png';
 import { Card } from 'react-bootstrap';
 import { CardColumns } from 'react-bootstrap';
 import { FleetBackend } from './api/FleetBackend';
+import { Button } from 'react-bootstrap';
+import { Modal, InputGroup, FormControl } from 'react-bootstrap';
 
 class TruckList extends Component {
 	constructor() {
 		super();
 		this.state = {
-            truck: []
-        };
+			truck: [],
+			show: false,
+		};
 	}
 	componentDidMount() {
-        FleetBackend()
-        .get('trucks/',{
-            name: this.state.name,
-            vin: this.state.vin,
-        })
-        .then((results) => {
-            this.setState({ truck: results.data })
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-    }
-    render() {
+		FleetBackend()
+			.get('trucks/', {})
+			.then((results) => {
+				this.setState({ truck: results.data });
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
+
+	handleModal() {
+		this.setState({ show: !this.state.show });
+	}
+	render() {
 		return (
-			<CardColumns>
-				<Card key=''>
-					<Card.Body className='p-6 mb-6 bg-warning text-white text-xl-center'>
-						<Card.Text className='text-white'>
-							<Link className='text-white' exact to={`/trucks/SingleTruck`}>
-								{' '}
-								{/* {`/trucks/${data.vin}`}*/}
-								<p>see more on details on this truck</p>
-								{/* {data.vin}{' '} */}
-								<img src={truckimage} className='img-fluid' alt='truck' />
-							</Link>
-						</Card.Text>
-					</Card.Body>
-				</Card>
-			</CardColumns>
+			<>
+				<CardColumns>
+					<Card key=''>
+						<Card.Body className='p-6 mb-6 bg-warning text-white text-xl-center'>
+							<Card.Text className='text-white'>
+								<Link className='text-white' exact to={`/trucks/SingleTruck`}>
+									{' '}
+									{/* {`/trucks/${data.vin}`}*/}
+									<p>see more on details on this truck</p>
+									{/* {data.vin}{' '} */}
+									<img src={truckimage} className='img-fluid' alt='truck' />
+								</Link>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</CardColumns>
+				<Button
+					onClick={() => {
+						this.handleModal();
+					}}>
+					add a new truck to the list yo
+				</Button>
+				<Modal show={this.state.show} onHide={() => this.handleModal()}>
+					<Modal.Header closeButton>
+						<Modal.Title>Add a new Truck to your Fleet</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Truck Name</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Truck Name'
+								aria-label='Truck Name'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Truck Vin</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Truck Vin'
+								aria-label='Truck Vin'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Truck Make</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Truck Make'
+								aria-label='Truck Make'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Truck Model</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Truck Model'
+								aria-label='Truck Model'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Plate</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Plate'
+								aria-label='Plate'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Status</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Status'
+								aria-label='Status'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>
+									Last Service
+								</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Date OfLast Service'
+								aria-label='Last Service'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Service Due</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='service due on'
+								aria-label='Service Due'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+						<InputGroup className='mb-3'>
+							<InputGroup.Prepend>
+								<InputGroup.Text id='basic-addon1'>Last Driver</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								type='text'
+								placeholder='Name of Last Driver'
+								aria-label='Last Driver'
+								aria-describedby='basic-addon1'
+							/>
+						</InputGroup>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button
+							onClick={() => {
+								this.handleModal();
+							}}>
+							Close
+						</Button>
+						<Button>Save Changes</Button>
+					</Modal.Footer>
+				</Modal>
+			</>
 		);
 	}
 }
