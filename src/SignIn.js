f8294358015c6948f8adce0d126b08e1723a02bd
cassioblu55 <cassioblubyrd@gmail.com>
2020-08-20 03:25:37 -0500

    added react to login and sign in

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,14 +1,15 @@
 // Create a page or modal that when clicked allows a user to enter their email and password and it allows them to be logged in.
 import React, { Component } from 'react';
 import { FleetBackend } from './api/FleetBackend';
-import { Redirect } from 'react-router-dom';
+import { Form, Button, Container, Col, Row } from 'react-bootstrap';
+import EmailValidator from 'email-validator';
 
 class SignIn extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			email: '',
-			password: '',
+			email: undefined,
+			password: undefined,
 			error: false,
 			errorMessage: undefined,
 		};
@@ -38,33 +39,54 @@ class SignIn extends Component {
 	};
 
 	render() {
-		const { email, password } = this.state;
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<label htmlFor='email'>Email</label>
-				<input
-					name='email'
-					type='text'
-					placeholder='Enter your email'
-					value={email}
-					onChange={this.handleChange}
-				/>
-				<label htmlFor='email'>Password</label>
-				<input
-					name='password'
-					type='password'
-					placeholder='Enter your password'
-					value={password}
-					onChange={this.handleChange}
-				/>
-				<button type='submit'>Login</button>
-				{this.state.error && (
-					<div style={{ color: 'red' }}>{this.state.errorMessage}</div>
-				)}
-			</form>
+			<Container>
+				<Row>
+					<Col>
+						<h1 className='text-center'>Login</h1>
+					</Col>
+				</Row>
+				<Form onSubmit={this.handleSubmit}>
+					<Form.Row>
+						<Form.Group controlId='email'>
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Email'
+								required
+								name='email'
+								isInvalid={
+									this.state.email !== undefined &&
+									!EmailValidator.validate(this.state.email)
+								}
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+					</Form.Row>
+					<Form.Row>
+						<Form.Group controlId='password'>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Password'
+								required
+								name='password'
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+					</Form.Row>
+					{this.state.error && (
+						<Row style={{marginBottom: "8px"}}>
+							<Col style={{ color: 'red' }}>Login Failed</Col>
+						</Row>
+					)}
+					<Button variant='primary' type='submit'>
+						Submit
+					</Button>
+				</Form>
+			</Container>
 		);
 	}
-
 }
 
 export default SignIn;
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,6 +1,6 @@
 // Make a page that will allow a user to enter their email and password so they can create an account, allow users to add their first and last name
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Col, Row } from 'react-bootstrap';
 import { FleetBackend } from './api/FleetBackend';
 import EmailValidator from 'email-validator';
 
@@ -40,62 +40,77 @@ class SignUp extends Component {
 
 	render() {
 		return (
-			<Form onSubmit={this.handleSubmit}>
-				<Form.Group controlId='name'>
-					<Form.Label>Enter your Full Name</Form.Label>
-					<Form.Control
-						type='text'
-						placeholder='Full Name'
-						required
-						name='name'
-						onChange={this.handleChange}
-					/>
-				</Form.Group>
-				<Form.Group controlId='email'>
-					<Form.Label>Email</Form.Label>
-					<Form.Control
-						type='text'
-						placeholder='Email'
-						required
-						name='email'
-						isInvalid={
-							this.state.email !== undefined &&
-							!EmailValidator.validate(this.state.email)
-						}
-						onChange={this.handleChange}
-					/>
-					<Form.Text className='text-muted'>
-						We'll never share your email with anyone else.
-					</Form.Text>
-				</Form.Group>
-				<Form.Group controlId='password'>
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						type='password'
-						placeholder='Password'
-						required
-						name='password'
-						onChange={this.handleChange}
-					/>
-				</Form.Group>
-				<Form.Group controlId='passwordConfirm'>
-					<Form.Label>Password Confirm</Form.Label>
-					<Form.Control
-						type='password'
-						placeholder='Password Confirm'
-						required
-						name='passwordConfirm'
-						onChange={this.handleChange}
-						isInvalid={
-							this.state.passwordConfirm !== undefined &&
-							this.state.password !== this.state.passwordConfirm
-						}
-					/>
-				</Form.Group>
-				<Button variant='primary' type='submit'>
-					Submit
-				</Button>
-			</Form>
+			<Container>
+				<Row>
+					<Col>
+						<h1 className='text-center'>Create Profile</h1>
+					</Col>
+				</Row>
+				<Form onSubmit={this.handleSubmit}>
+					<Form.Row>
+						<Form.Group controlId='name'>
+							<Form.Label>Enter your Full Name</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Full Name'
+								required
+								name='name'
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+					</Form.Row>
+					<Form.Row>
+						<Form.Group controlId='email'>
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								type='text'
+								placeholder='Email'
+								required
+								name='email'
+								isInvalid={
+									this.state.email !== undefined &&
+									!EmailValidator.validate(this.state.email)
+								}
+								onChange={this.handleChange}
+							/>
+							<Form.Text className='text-muted'>
+								We'll never share your email with anyone else.
+							</Form.Text>
+						</Form.Group>
+					</Form.Row>
+					<Form.Row>
+						<Form.Group controlId='password'>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Password'
+								required
+								name='password'
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+					</Form.Row>
+					<Form.Row>
+						<Form.Group controlId='passwordConfirm'>
+							<Form.Label>Password Confirm</Form.Label>
+							<Form.Control
+								type='password'
+								placeholder='Password Confirm'
+								required
+								name='passwordConfirm'
+								onChange={this.handleChange}
+								isInvalid={
+									this.state.passwordConfirm !== undefined &&
+									this.state.password !== this.state.passwordConfirm
+								}
+							/>
+						</Form.Group>
+					</Form.Row>
+					<Button variant='primary' type='submit'>
+						Submit
+					</Button>
+				</Form>
+			</Container>
 		);
 	}
 }
